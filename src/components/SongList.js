98eae29085db6d614abccc3d58e794371e320f9f
Tyler <tylerhines1992@gmail.com>
2019-09-25 19:46:31 -0400

    setup connect on SongList

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { connect } from "react-redux";
 
 class SongList extends React.Component {
 	render() {
@@ -6,4 +7,9 @@ class SongList extends React.Component {
 	}
 }
 
-export default SongList;
+// Return an object as props on Component in connect param
+const mapStateToProps = state => {
+	return { songs: state.songs };
+};
+
+export default connect(mapStateToProps)(SongList);
