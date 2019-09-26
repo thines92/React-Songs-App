884ffafc89fa3129f1bd2c71963acd216473f516
Tyler <tylerhines1992@gmail.com>
2019-09-25 20:02:37 -0400

    wire selectSong action in SongList

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { selectSong } from "../actions";
 
 class SongList extends React.Component {
 	renderList() {
@@ -7,7 +8,12 @@ class SongList extends React.Component {
 			return (
 				<div className="item" key={song.title}>
 					<div className="right floated content">
-						<button className="ui button primary">Select</button>
+						<button
+							className="ui button primary"
+							onClick={() => this.props.selectSong(song)}
+						>
+							Select
+						</button>
 					</div>
 					<div className="content">{song.title}</div>
 				</div>
@@ -22,7 +28,11 @@ class SongList extends React.Component {
 
 // Return an object as props on Component in connect param
 const mapStateToProps = state => {
+	console.log(state);
 	return { songs: state.songs };
 };
 
-export default connect(mapStateToProps)(SongList);
+export default connect(
+	mapStateToProps,
+	{ selectSong }
+)(SongList);
